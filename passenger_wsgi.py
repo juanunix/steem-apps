72f7b80af580bb7b4cba0017e771be5951ec4b93
Kasper Fredenslund <kasper.fredenslund@gmail.com>
2017-09-19 21:13:40 +0200

    readme relocation

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append('./steem-tag-search')
-
-INTERP = "/home/kasfre2/steem.kasperfred.com/venv/bin/python3"
-#INTERP is present twice so that the new Python interpreter knows the actual executable path
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
-import app as application
